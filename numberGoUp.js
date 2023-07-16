03dc930b43042beed11c5511fb35a4bb1470c830
Jonas Ewert <JonasEw@gmail.com>
2023-07-16 22:41:01 +0200

    less clicks still dumb

--- numberGoUp.js
+++ numberGoUp.js
@@ -26,9 +26,9 @@ growButton.onclick = function incrementUp() {
   if (mostBelovedNumber > upgrade1Cost) {
     mostBelovedNumber = mostBelovedNumber - upgrade1Cost;
     updateNumber();
-    upgrade1Cost = upgrade1Cost * 1.0375;
+    upgrade1Cost = upgrade1Cost * 1.80375;
     upgrade1CostElem.textContent = `Cost: ${upgrade1Cost.toFixed(2)}`;
-    growIncrement = growIncrement * 1.047;
+    growIncrement = growIncrement * 1.8470;
     incrementScoreElem.textContent = "Growth: " + growIncrement.toFixed(2);
   }
   else {
